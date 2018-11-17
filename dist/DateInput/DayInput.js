a1a0221b82cdf5f9b002ed61cce23aff5d56547e
Marco Ancona <marco.ancona@inf.ethz.ch>
2018-11-17 15:56:23 +0100

    push dist

---
+++ dist/DateInput/DayInput.js
@@ -0,0 +1,110 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _mergeClassNames = require('merge-class-names');
+
+var _mergeClassNames2 = _interopRequireDefault(_mergeClassNames);
+
+var _propTypes3 = require('../shared/propTypes');
+
+var _utils = require('../shared/utils');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var select = function select(element) {
+  return element && element.select();
+};
+
+var DayInput = function (_PureComponent) {
+  _inherits(DayInput, _PureComponent);
+
+  function DayInput() {
+    _classCallCheck(this, DayInput);
+
+    return _possibleConstructorReturn(this, (DayInput.__proto__ || Object.getPrototypeOf(DayInput)).apply(this, arguments));
+  }
+
+  _createClass(DayInput, [{
+    key: 'render',
+    value: function render() {
+      var _props = this.props,
+          className = _props.className,
+          disabled = _props.disabled,
+          itemRef = _props.itemRef,
+          value = _props.value,
+          onChange = _props.onChange,
+          onKeyDown = _props.onKeyDown,
+          required = _props.required,
+          showLeadingZeros = _props.showLeadingZeros;
+
+      var v = parseInt(value, 10) ? parseInt(value, 10).toString().slice(-2) : '';
+      if (showLeadingZeros && v.length === 1) v = '0' + v;
+      var name = 'day';
+
+      return [_react2.default.createElement('input', {
+        key: 'day',
+        className: (0, _mergeClassNames2.default)(className + '__input', className + '__day'),
+        disabled: disabled,
+        name: name,
+        onChange: onChange,
+        onFocus: function onFocus(event) {
+          return select(event.target);
+        },
+        onMouseDown: function onMouseDown(event) {
+          event.preventDefault();select(event.target);
+        },
+        onKeyDown: onKeyDown,
+        onKeyUp: function onKeyUp(event) {
+          return (0, _utils.updateInputWidth)(event.target);
+        },
+        placeholder: '--',
+        ref: function ref(_ref) {
+          if (_ref) {
+            (0, _utils.updateInputWidth)(_ref);
+          }
+
+          if (itemRef) {
+            itemRef(_ref, name);
+          }
+        },
+        required: required,
+        value: v
+      })];
+    }
+  }]);
+
+  return DayInput;
+}(_react.PureComponent);
+
+exports.default = DayInput;
+
+
+DayInput.propTypes = {
+  className: _propTypes2.default.string.isRequired,
+  disabled: _propTypes2.default.bool,
+  itemRef: _propTypes2.default.func,
+  onChange: _propTypes2.default.func,
+  onKeyDown: _propTypes2.default.func,
+  required: _propTypes2.default.bool,
+  showLeadingZeros: _propTypes2.default.bool,
+  value: _propTypes2.default.number
+};
